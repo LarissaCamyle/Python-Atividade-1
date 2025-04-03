075cca630a28f13121d8684c2dae92e0d85842e7
Larissa Camyle da Silva <lsilva@focus.ind.br>
2025-04-03 10:28:54 -0300

    Criando programa

---
+++ App.py
@@ -0,0 +1,10 @@
+print("======Restaurante======\n")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print('Você escolheu essa opção = ', opcao_escolhida)
