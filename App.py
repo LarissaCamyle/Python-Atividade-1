c43658baa8e5e526bce175793b073e10345fdfc3
Larissa Camyle da Silva <lsilva@focus.ind.br>
2025-04-07 12:02:38 -0300

    Criando alterações para o programa

--- App.py
+++ App.py
@@ -1,4 +1,23 @@
-print("======Restaurante======\n")
+import os
+
+#FUNÇÕES---------------------------------------------------
+
+def encerrar_app ():
+    print("""
+      
+      
+      Deseja sair?
+      
+      """)
+
+#CÓDIGO----------------------------------------------------
+
+print(""" 
+▒█▀▀█ █▀▀ █▀▀ ▀▀█▀▀ █▀▀█ █░░█ █▀▀█ █▀▀█ █▀▀▄ ▀▀█▀▀ █▀▀ 
+▒█▄▄▀ █▀▀ ▀▀█ ░░█░░ █▄▄█ █░░█ █▄▄▀ █▄▄█ █░░█ ░░█░░ █▀▀ 
+▒█░▒█ ▀▀▀ ▀▀▀ ░░▀░░ ▀░░▀ ░▀▀▀ ▀░▀▀ ▀░░▀ ▀░░▀ ░░▀░░ ▀▀▀
+      
+      """)
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
@@ -7,4 +26,29 @@ print('4. Sair\n\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
 
-print('Você escolheu essa opção = ', opcao_escolhida)
+#limpa programa antes de exibir as opções
+os.system('cls')
+
+if opcao_escolhida == "1" :
+    print("""======CADASTRO======
+    
+    
+    1.
+    2. Deseja sair""")
+
+elif opcao_escolhida == "2" :
+    print("""======LISTA DE RESTAURANTES======
+    
+    
+    1.
+    2. Deseja sair""")
+
+elif opcao_escolhida == "3" : 
+    print("""======ATIVAR RESTAURANTE======
+    
+    
+    1.
+    2. Deseja sair""")
+
+elif opcao_escolhida == "4" :
+    encerrar_app()
