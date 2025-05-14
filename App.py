374b86bcd5943bb35298d3e186c95bc0e18b3752
Larissa Camyle da Silva <lsilva@focus.ind.br>
2025-05-14 10:15:25 -0300

    Adicionando docstring no código.

--- App.py
+++ App.py
@@ -2,7 +2,10 @@ import os
  
 #FUNÇÕES PRINCIPAIS---------------------------------------------------
 
-def iniciar_programa () :  
+def iniciar_programa () : 
+    ''' Essa função é resposável por printar a tela inicial do programa'''
+
+    os.system('cls')
     print(""" 
     ▒█▀▀█ █▀▀ █▀▀ ▀▀█▀▀ █▀▀█ █░░█ █▀▀█ █▀▀█ █▀▀▄ ▀▀█▀▀ █▀▀ 
     ▒█▄▄▀ █▀▀ ▀▀█ ░░█░░ █▄▄█ █░░█ █▄▄▀ █▄▄█ █░░█ ░░█░░ █▀▀ 
@@ -16,6 +19,10 @@ def iniciar_programa () :
     print('4. Sair\n\n')
 
 def printar_opcoes():
+    ''' Essa função é resposável por printar as opções do menu e ir para o menu desejado
+    
+    Input : A opção escolhida no menu 
+    '''
     opcao_escolhida = input('Escolha uma opção: ')
 
     if opcao_escolhida == "1" :
@@ -27,35 +34,64 @@ def printar_opcoes():
     elif opcao_escolhida == "4" :
         encerrar_app()
     else:
-        opcao_invalida()
+        opcao_invalida('')
 
 
 #pequenas tarefas
 
-def encerrar_app ():
+def encerrar_app():
+    ''' Essa função é resposável por encerrar o programa'''
+
     #limpar terminal
     os.system('cls')
     exit()
 
 def voltar_para_comeco():
-    input("""                   
+    ''' Essa função é resposável por voltar a tela inicial do programa'''
+
+    input("""
+                             
         Digite qualquer tecla para voltar ao início = 
     """)
     main()
 
-def opcao_invalida():
-    input(""" 
-        Opção inválida!
-    """)
+def opcao_invalida(texto):
+    ''' Essa função é resposável por printar uma mensagem de erro, a mensagem pode ser personalizada ou automática
+    
+    Input : A mensagem de erro (opcional)
+    '''
+
+    if texto == '':
+        input(""" 
+            Opção inválida!
+        """)
+    else:
+        input(f""" 
+            {texto}
+        """)
     voltar_para_comeco()
 
 def exibir_titulo(titulo):
+    ''' Essa função é resposável por printar um título
+    
+    Input : Título
+    '''
+
     os.system('cls')
     decoracao = '═' * (len(titulo))
 
     print(f'                {decoracao}')
     print(f'                {titulo}')
     print(f'                {decoracao}\n')
+
+def validar_input(input):
+    ''' Essa função é resposável por verificar se o input realizado está vazio
+    
+    Input : o input realizado pelo usuário
+    '''
+    
+    if input == '':
+        opcao_invalida('Entrada de dados vazia, favor digitar corretamente as informações!') 
 
 
 #Cadastro-------------------
@@ -65,11 +101,19 @@ restaurantes = [{'nome':'Poletto', 'categoria':'Massas', 'status':True},
                 {'nome':'Madero', 'categoria':'Massas', 'status':False}]
 
 def cadastrar_novo_restaurante():
+    ''' Essa função é resposável por criar um novo restaurante
+    
+    Input : Nome do restaurante e categoria do restaurante
+    Output: Salva o restaurante novo na lista de restaurantes
+    '''
+
     exibir_titulo("CADASTRO DE NOVOS RESTAURANTES")
 
     #entrada de dados
     nome = input('Digite o nome do restaurante = ')
+    validar_input(nome)
     categoria = input('Digite a categoria = ')
+    validar_input(categoria)
 
     #armazenamento de dados
     dados_do_restaurante = {'nome':nome, 'categoria':categoria, 'status':False}
@@ -85,14 +129,19 @@ def cadastrar_novo_restaurante():
 #listar--------------------
 
 def listar_restaurantes():
+    ''' Essa função é resposável por listar todos os restaurantes'''
+
     exibir_titulo("LISTA DE RESTAURANTES")
+
+    #titulo da tabela
+    print(f'{'  NOME'.ljust(20)}  {'   Categoria'.ljust(20)}  {'   Status'.ljust(20)}\n')
 
     for restaurante in restaurantes:
         nome = restaurante['nome']
         categoria = restaurante['categoria']
         #se o status é true printa ativo, e se for falso printa desativado
         status = 'Ativo ' if restaurante['status'] else 'Desativado'
-        print(f'NOME = {nome}\nCATEGORIA = {categoria}\nSTATUS = {status}\n\n')
+        print(f'★ {nome.ljust(20)} ║ {categoria.ljust(20)} ║ {status.ljust(20)}')
 
     voltar_para_comeco()
 
@@ -100,6 +149,12 @@ def listar_restaurantes():
 #alterar------------------
 
 def alterar_status_restaurante():
+    ''' Essa função é resposável por alterar o status do restaurante
+    
+    Input : Nome do restaurante
+    Output : Status do restaurante 
+    '''
+
     exibir_titulo("ALTERAR STATUS DO RESTAURANTE")
 
     nome = input("Digite o nome do restaurante que deseja alterar o status = ")
@@ -111,13 +166,13 @@ def alterar_status_restaurante():
             restaurante_encontrado = True
             #coloca o status contrario do q ja foi armazenado
             restaurante['status'] = not restaurante['status']
-            if restaurante['status']:
-                mensagem = f'\nO status do restaurante {restaurante["nome"]} foi alterado com sucesso!\n'
-            else:
-                mensagem = f'\nO status do restaurante {restaurante["nome"]} não foi alterado!\n'
+
+            status_restaurante = 'Ativo' if restaurante['status'] else 'Desativado'
+
+            mensagem = f'\nO status do restaurante {restaurante["nome"]} foi alterado para {status_restaurante} com sucesso!\n'
             print(mensagem)
-        else:
-            opcao_invalida()
+    if restaurante_encontrado == False:
+        opcao_invalida('\nRestaurante não encontrado!')
 
     voltar_para_comeco()
 
@@ -128,12 +183,15 @@ def alterar_status_restaurante():
 
 #transforma o App.py em main
 def main():
+    ''' Essa função é resposável pela ordem da execução do programa'''
+    
     os.system('cls')
     iniciar_programa()
     printar_opcoes()
     encerrar_app()
 
 if __name__ == '__main__':
+    ''' Transforma o arquivo atual (App.py) no arquivo principal do código'''
     main()
 
 
